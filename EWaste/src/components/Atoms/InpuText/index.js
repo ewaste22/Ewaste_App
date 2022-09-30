8e35ff31926f1ca890855d0b47f8c4a59230517b
bayucucanh <bayucucan36@gmail.com>
2022-09-30 21:29:59 +0700

    Testing Redux Success

---
+++ EWaste/src/components/Atoms/InpuText/index.js
@@ -0,0 +1,36 @@
+import { StyleSheet, Text, TextInput, View } from 'react-native'
+import React, {useState} from 'react'
+
+const InputText = ({placeholder, multiline, style, type, maxLength, onChangeText, value, error, name, onBlur, secureTextEntry}) => {
+  const [isFocus, setIsFocus] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      <TextInput 
+        onFocus={() => setIsFocus(true)}
+        onBlur={onBlur}
+        multiline={multiline}
+        onChangeText={onChangeText}
+        value={value}
+        name={name}
+        secureTextEntry={secureTextEntry}
+        maxLength={maxLength}
+        keyboardType={type}
+        placeholder={placeholder}
+        // style={style.inputs}
+      />
+    </View>
+  )
+}
+
+export default InputText
+
+const styles = StyleSheet.create({
+  container: {
+      borderRadius: 15,
+      borderWidth: 2,
+      justifyContent: 'flex-start',
+      paddingHorizontal: 10,
+      flexDirection: 'row',
+  }
+})
